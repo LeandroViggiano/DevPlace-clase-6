f1b317226d24df706042580510872ca0a8454751
LeoSebastian <leosebastiang19@gmail.com>
2022-10-24 16:28:57 -0300

    comentarios 2

---
+++ main.js
@@ -0,0 +1,12 @@
+function potenciaNumerica(){
+    n = parseInt(prompt("Ingrese un numero y se lo elevara a la potencia:"))
+    p = parseInt(prompt("Muy bien!\nAhora ingrese el numero al que lo quiere elevar:"))
+
+    if ((n > 0) && (p > 0)){
+        potencia = Math.pow(n, p)
+        alert("Perfecto.\n" + n + " elevado a la " +  p + "es" + potencia)
+    }
+    else {
+        alert("No se permite numeros negativos")
+    }
+}
